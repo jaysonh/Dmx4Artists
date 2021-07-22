21b4e840d17ba3a2a46b2c286f3f673261220a85
jaysonh <mail@jaysonh.com>
2021-07-22 18:55:25 +0930

    added extra checking for dll file on windows

--- src/com/jaysonh/dmx4artists/WINValidator.java
+++ src/com/jaysonh/dmx4artists/WINValidator.java
@@ -1,6 +1,7 @@
 package com.jaysonh.dmx4artists;
 
 import java.io.File;
+import java.util.StringTokenizer;
 
  /** 
   * Checks a Windows system to see if it has the correct settings to run the library
@@ -13,18 +14,57 @@ import java.io.File;
 
 public class WINValidator
 {
+	/**
+	   * Check if windows can find the ftd2xx.dll file
+	   *  
+	   * @return boolean if the function can find the ftd2x.dll file
+	   */ 
 	public boolean isValid()
 	{
-		File dllFile = new File("C:/Windows/System32/ftd2xx.dll");
-		
-		if( dllFile.exists())
-		{
-			System.out.println("Found ftd2xx.dll");
-			return true;
-		}else
-		{
-			System.out.println("Cannot find ftd2xx.dll");
-			return false;
-		}
+		boolean exists    = false;
+	    String  foundPath = "";
+	   
+	    // Get the system paths for DLL files
+	    String property = System.getProperty("java.library.path");
+	    StringTokenizer parser = new StringTokenizer(property, ";");
+	    
+	    // Loop through all the folders that the system searches for dll files in
+	    while (parser.hasMoreTokens())
+	    {
+	    	// Check if ftd2xx.dll is in this folder
+	        String searchPath   = parser.nextToken() + "\\ftd2xx.dll";
+	        
+	        if( new File( searchPath ).exists() )
+	        {
+	          exists    = true;
+	          foundPath = searchPath;
+	        }
+	    }
+	     
+	    if(exists)
+	    {      
+	         System.out.println("found in: " + foundPath);
+	         
+	         return true;
+	    } else
+	    {
+	         System.err.println("Cannot find ftd2xx.dll, please copy from winDependencies to one of these folders:");
+	           
+	         StringTokenizer dllFolders = new StringTokenizer( property, ";" );
+	         
+	         // If there are no folders in the System path
+	         if( dllFolders.countTokens() <= 0 )
+	         {
+	             System.err.println( "Error! No search paths for DLL files?" );  
+	         }
+	      
+	         // Print each folder to the console
+	         while ( dllFolders.hasMoreTokens())
+	         {
+	              System.err.println( dllFolders.nextToken() );
+	         }
+	         
+	         return false;
+	      }
 	}
 }
